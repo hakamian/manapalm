810650364dea86f6640c5e21cf76934dbcd03fef
Mana CTO <info@manapalm.com>
2026-02-05 15:43:47 +0330

    Install react-hot-toast and add Toaster provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,11 +4,21 @@ import React from 'react';
 import { AppProvider } from '../AppContext';
 import { CartProvider } from '../contexts/CartContext';
 
+import { Toaster } from 'react-hot-toast';
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AppProvider>
       <CartProvider>
         {children}
+        <Toaster position="bottom-center" toastOptions={{
+          style: {
+            background: '#333',
+            color: '#fff',
+            direction: 'rtl',
+            fontFamily: 'inherit'
+          }
+        }} />
       </CartProvider>
     </AppProvider>
   );
